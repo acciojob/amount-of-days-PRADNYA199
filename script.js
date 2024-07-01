b0ff8f097b4e93e5144781425c05f035f2738282
Pradnya Chandrakant Bhosale <67332759+PRADNYA199@users.noreply.github.com>
2024-07-01 09:14:21 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+function daysOfAYear(year) {
+const isLeapYear = (year % 4 === 0);
+  
+  // If it's divisible by 100, check if it's also divisible by 400 to confirm leap year
+  if (year % 100 === 0) {
+    if (year % 400 === 0) {
+      isLeapYear = true;
+    } else {
+      isLeapYear = false;
+    }
+  }
+  
+  // Return 366 days for leap years, otherwise 365 days
+  return isLeapYear ? 366 : 365;
+}
